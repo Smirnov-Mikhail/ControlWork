958b67d3414da1dafdd1d5ee23052d840069eb0e
Smirnov Mikhail <sm_m_a@mail.ru>
2015-04-03 12:59:59 +0300

    кр

---
+++ 4sem/task1/ConsoleApplication1/SumOfFibNumbers.fs
@@ -0,0 +1,7 @@
+﻿let fib = Seq.unfold
+            (fun (u, v) -> Some(u, (u + v, u)))
+            (1, 1)
+
+let sum = List.sum (Seq.take 10 (Seq.filter (fun x -> (x % 2  = 0 && x < 1000000)) fib) |> Seq.toList)
+
+printfn "%A" sum
---
+++ 4sem/task3/task3/Program.fs
@@ -0,0 +1,28 @@
+﻿exception EmptyQueueException of string
+
+type QueueElement<'a> =
+       struct        
+          val Value: 'a
+          val Priority: int
+          new(value: 'a, priority: int) = { Value = value; Priority = priority }
+       end
+
+type Queue<'a>() =
+    let mutable list : 'QueueElement list = []
+
+    member this.isEmpty = list.Length = 0
+
+    member this.push(value, priority) = 
+        let element = new QueueElement<'a> (value, priority)
+        list <- list @ [element]
+
+    member this.pop =
+        if (list.IsEmpty) then
+            raise (EmptyQueueException "failed pop method. The Queue is empty!")
+
+        let mutable indexMaxPriority = 0;
+        for i = 0 to list.Length - 1 do
+            if (list.Item(i).Priority > list.Item(indexMaxPriority).Priority) then
+                indexMaxPriority <- i
+        // не успел удалить элемент
+        list.Item(indexMaxPriority).Value
